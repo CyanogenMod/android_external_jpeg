a6567963ad9040df76b53fac9279e74e0864fc70
Jean-Baptiste Queru <jbq@google.com>
2009-11-12 18:45:29 -0800

    eclair snapshot

--- jmorecfg.h
+++ jmorecfg.h
@@ -20,7 +20,7 @@
 #define ANDROID_RGB
 
 #ifdef ANDROID_RGB
-#define PACK_SHORT_565(r,g,b)  ((((r)<<8)&0xf800)|(((g)<<3)&0x7C0)|((b)>>3))
+#define PACK_SHORT_565(r,g,b)  ((((r)<<8)&0xf800)|(((g)<<3)&0x7E0)|((b)>>3))
 #define PACK_TWO_PIXELS(l,r)   ((r<<16) | l)
 #define PACK_NEED_ALIGNMENT(ptr) (((int)(ptr))&3)
 #define WRITE_TWO_PIXELS(addr, pixels) do {     \
