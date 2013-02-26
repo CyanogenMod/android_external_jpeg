7fb4687279b703d9f866cefac5fc6967fb81068a
Angus Kong <shkong@google.com>
2013-02-25 16:13:09 -0800

    Add static library target.
    Change-Id: Ibd4dcf7aa46c4198d7baeecf19fd330e647abe6d

--- Android.mk
+++ Android.mk
@@ -4,34 +4,31 @@ include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
 
 LOCAL_SRC_FILES := \
-	jcapimin.c jcapistd.c jccoefct.c jccolor.c jcdctmgr.c jchuff.c \
-	jcinit.c jcmainct.c jcmarker.c jcmaster.c jcomapi.c jcparam.c \
-	jcphuff.c jcprepct.c jcsample.c jctrans.c jdapimin.c jdapistd.c \
-	jdatadst.c jdatasrc.c jdcoefct.c jdcolor.c jddctmgr.c jdhuff.c \
-	jdinput.c jdmainct.c jdmarker.c jdmaster.c jdmerge.c jdphuff.c \
-	jdpostct.c jdsample.c jdtrans.c jerror.c jfdctflt.c jfdctfst.c \
-	jfdctint.c jidctflt.c jidctfst.c jidctint.c jidctred.c jquant1.c \
-	jquant2.c jutils.c jmemmgr.c armv6_idct.S
+    jcapimin.c jcapistd.c jccoefct.c jccolor.c jcdctmgr.c jchuff.c \
+    jcinit.c jcmainct.c jcmarker.c jcmaster.c jcomapi.c jcparam.c \
+    jcphuff.c jcprepct.c jcsample.c jctrans.c jdapimin.c jdapistd.c \
+    jdatadst.c jdatasrc.c jdcoefct.c jdcolor.c jddctmgr.c jdhuff.c \
+    jdinput.c jdmainct.c jdmarker.c jdmaster.c jdmerge.c jdphuff.c \
+    jdpostct.c jdsample.c jdtrans.c jerror.c jfdctflt.c jfdctfst.c \
+    jfdctint.c jidctflt.c jidctfst.c jidctint.c jidctred.c jquant1.c \
+    jquant2.c jutils.c jmemmgr.c armv6_idct.S
 
 ifeq (,$(TARGET_BUILD_APPS))
 # building against master
 # use ashmem as libjpeg decoder's backing store
 LOCAL_CFLAGS += -DUSE_ANDROID_ASHMEM
 LOCAL_SRC_FILES += \
-	jmem-ashmem.c
-
-LOCAL_SHARED_LIBRARIES := \
-	libcutils
+    jmem-ashmem.c
 else
 # unbundled branch, built against NDK.
 LOCAL_SDK_VERSION := 17
 # the original android memory manager.
 # use sdcard as libjpeg decoder's backing store
 LOCAL_SRC_FILES += \
-	jmem-android.c
+    jmem-android.c
 endif
 
-LOCAL_CFLAGS += -DAVOID_TABLES 
+LOCAL_CFLAGS += -DAVOID_TABLES
 LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays
 #LOCAL_CFLAGS += -march=armv6j
 
@@ -46,6 +43,27 @@ else
 LOCAL_CFLAGS += -DANDROID_ARMV6_IDCT
 endif
 
-LOCAL_MODULE:= libjpeg
+LOCAL_MODULE := libjpeg_static
+
+include $(BUILD_STATIC_LIBRARY)
+
+
+
+# Build shared library
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libjpeg
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_WHOLE_STATIC_LIBRARIES = libjpeg_static
+
+ifeq (,$(TARGET_BUILD_APPS))
+LOCAL_SHARED_LIBRARIES := \
+    libcutils
+else
+# unbundled branch, built against NDK.
+LOCAL_SDK_VERSION := 17
+endif
 
 include $(BUILD_SHARED_LIBRARY)
